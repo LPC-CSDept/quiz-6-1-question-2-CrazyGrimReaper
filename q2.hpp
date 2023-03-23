49b7ff80be76bb32b9112f1d7ab025a895394bff
Joshua Aguilar <aguilar60080@gmail.com>
2023-03-22 21:11:16 -0700

    Final push

--- q2.hpp
+++ q2.hpp
@@ -5,3 +5,33 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+void getInput(int &n1, int &n2, int &n3)
+{
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+}
+
+int  findMin(int n1, int n2, int n3)
+{
+    int smallestNum;
+    if(n1 < n2 && n1 < n3)
+	{
+		smallestNum = n1;
+	}
+	else if(n2 < n3)
+	{
+		smallestNum = n2;
+	}
+	else
+	{
+		smallestNum = n3;
+	}	
+    return smallestNum;
+}
+
+void printResult(int n1, int n2, int n3, int min)
+{
+    cout << n1 << " " << n2 << " "  << n3 << endl;
+    cout << min;
+}
